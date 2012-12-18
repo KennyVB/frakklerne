1429c20cb8857109c4340323f1efc95d34818624
kennyvb <kenny@kennyvb.dk>
2012-12-18 16:33:21 +0100

    finished adding FriendlyId slug

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,4 +2,10 @@ class Gallery < ActiveRecord::Base
   attr_accessible :name
   validates :name,  :presence => true
   has_many :pictures, :dependent => :destroy
+  
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :history]
+  def should_generate_new_friendly_id?
+      new_record?
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121218112001) do
+ActiveRecord::Schema.define(:version => 20121218153013) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -23,11 +23,25 @@ ActiveRecord::Schema.define(:version => 20121218112001) do
 
   add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
 
+  create_table "friendly_id_slugs", :force => true do |t|
+    t.string   "slug",                         :null => false
+    t.integer  "sluggable_id",                 :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], :name => "index_friendly_id_slugs_on_slug_and_sluggable_type", :unique => true
+  add_index "friendly_id_slugs", ["sluggable_id"], :name => "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], :name => "index_friendly_id_slugs_on_sluggable_type"
+
   create_table "galleries", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "slug"
   end
+
+  add_index "galleries", ["slug"], :name => "index_galleries_on_slug"
 
   create_table "pictures", :force => true do |t|
     t.string   "name"
