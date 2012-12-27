2e50b0172e019e1e1195e4f5e49156c57929b4a1
kennyvb <kenny@kennyvb.dk>
2012-12-27 18:15:29 +0100

    changed image types

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,5 @@
 class Asset < ActiveRecord::Base
   attr_accessible :asset_content_type, :asset_file_name, :asset_file_size, :asset_updated_at, :post_id, :asset
   belongs_to :post
-  has_attached_file :asset, :styles => { :large => "640x480>", :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :asset, :styles => { :large => "640x480>", :thumb => "100x100>" }
 end
